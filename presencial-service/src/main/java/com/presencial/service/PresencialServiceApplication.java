82cfdfbc607f1d2eceac0b43046949830bdde879
BONIFAZ282 <bonihd48@gmail.com>
2023-07-26 17:25:01 -0500

    eureka-server

--- presencial-service/src/main/java/com/presencial/service/PresencialServiceApplication.java
+++ presencial-service/src/main/java/com/presencial/service/PresencialServiceApplication.java
@@ -2,8 +2,10 @@ package com.presencial.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PresencialServiceApplication {
 
 	public static void main(String[] args) {
--- universidad-service/src/main/java/com/universidad/service/UniversidadServiceApplication.java
+++ universidad-service/src/main/java/com/universidad/service/UniversidadServiceApplication.java
@@ -2,10 +2,12 @@ package com.universidad.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UniversidadServiceApplication {
 
 	public static void main(String[] args) {
